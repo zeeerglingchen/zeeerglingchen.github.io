2e2656a137cf61e4bb8db6d889a59aa3a5ef3bc8
chenyang lyu <lvchen16820@163.com>
2020-03-13 14:36:41 -0400

    march

---
+++ js/scrollandHide.js
@@ -0,0 +1,16 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navigation").style.top = "0";
+  } else {
+    document.getElementById("navigation").style.top = "-300px";
+  }
+  prevScrollpos = currentScrollPos;
+
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+
+}
